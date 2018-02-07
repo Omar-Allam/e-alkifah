9bd26836a2c66891f3e6a64cafb68ce43841e032
omar <omar.coder007@gmail.com>
2018-02-07 22:27:47 +0300

    remove list

--- resources/views/course/announce.blade.php
+++ resources/views/course/announce.blade.php
@@ -11,15 +11,6 @@
             <input type="text" class="form-control" id="course_subject" placeholder="{{t('المدرب')}}">
         </div>
         <div class="form-group" dir="rtl">
-            <label for="exampleInputPassword1">{{t('التصنيفات')}}</label>
-            <select class="form-control" multiple size="5">
-                <option>{{t('إدارة أعمال')}}</option>
-                <option>{{t('إقتصاد')}}</option>
-                <option>{{t('برمجة')}}</option>
-                <option>{{t('تنمية بشرية')}}</option>
-            </select>
-        </div>
-        <div class="form-group" dir="rtl">
             <label for="exampleInputFile">{{t('وصف للدورة')}}</label>
             <textarea class="form-control" rows="9"></textarea>
         </div>
--- resources/views/course/create.blade.php
+++ resources/views/course/create.blade.php
@@ -6,15 +6,7 @@
             <label for="course_subject">{{t('عنوان الدورة')}}</label>
             <input type="text" class="form-control" id="course_subject" placeholder="{{t('عنوان الدورة')}}">
         </div>
-        <div class="form-group" dir="rtl">
-            <label for="exampleInputPassword1">{{t('التصنيفات')}}</label>
-            <select class="form-control" multiple size="5">
-                <option>{{t('إدارة أعمال')}}</option>
-                <option>{{t('إقتصاد')}}</option>
-                <option>{{t('برمجة')}}</option>
-                <option>{{t('تنمية بشرية')}}</option>
-            </select>
-        </div>
+
         <div class="form-group" dir="rtl">
             <label for="exampleInputFile">{{t('وصف للدورة')}}</label>
             <textarea class="form-control" rows="9"></textarea>
